a172ba6c8ed7cbbc5f95b475729efc5a2e83efba
Eu <1165080170@qq.com>
2022-01-04 13:33:56 +0800

    完成了主页功能的开发

---
+++ assets/js/baseapi.js
@@ -0,0 +1,18 @@
+// 每次调用请求前,都会先调用这个函数
+$.ajaxPrefilter(function(option) {
+    option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
+    // 统一为有权限的接口设置请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ' '
+        }
+    }
+    // 全局挂载complete回调
+    option.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+        console.log(option.url);
+    }
+})
